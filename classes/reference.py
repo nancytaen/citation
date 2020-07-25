17a97a89511166021e9a23ba448809f44c170e14
nancytaen <taen.heizo8@gmail.com>
2020-07-25 18:02:33 -0400

    ordered citation

--- classes/reference.py
+++ classes/reference.py
@@ -9,7 +9,7 @@ class Reference:
     represents the entire reference section
     """
     def __init__(self):
-        self.reference_list = []
+        self.reference_list = {}
     
     def format_list(self, raw_list):
         """
@@ -17,20 +17,27 @@ class Reference:
         """
         with ThreadPoolExecutor(max_workers=50) as executor:
             for i in range(len(raw_list)):
-                executor.submit(self.create_citation, raw_list[i])
+                executor.submit(self.create_citation, raw_list[i], i)
+        self.sort_dict()
+
+    def sort_dict(self):
+        """
+        sort reference dict in order of keys
+        """
+        self.reference_list = dict(sorted(self.reference_list.items()))
     
-    def create_citation(self, source):
+    def create_citation(self, source, num):
         """
         helper function; create and format single url using IEEE
-        """        
+        """    
         source = source.strip(' ')
-        if source:
+        if source:    
             c = Citation(source)
-            self.reference_list.append(c.format())
+            self.reference_list[num + 1] = c.format()
     
     def print_list(self):
-        for i in range(len(self.reference_list)):
-            print("[{}] {}\n".format(str(i=1), self.reference_list[i]))
+        for num, citation in self.reference_list.items():
+            print("[{}] {}\n".format(str(num), citation))
 
     def export_to_text(self):
         with open('referece.txt', 'w') as sys.stdout:
@@ -46,4 +53,4 @@ class Reference:
 # "https://www.economist.com/middle-east-and-africa/2020/07/18/covid-19-has-throttled-south-africas-economy", 
 # "https://www.economist.com/business/2020/07/18/the-varying-american-fortunes-of-grindr-and-blued"
 # ])
-# r.export_to_text()
+# r.print_list()
